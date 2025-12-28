e95fe64b643fb5ebb8afd770d2e124c635f57724
SorerBOT <alonfiller@gmail.com>
2025-12-28 21:06:28 +0200

    FEAT creating shoe table

---
+++ Ass2_DB/q2_1.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS shoe (
+                shoe_id INT AUTO_INCREMENT PRIMARY KEY,
+                shoe_name VARCHAR(31) NOT NULL,
+                price SMALLINT NOT NULL
+                )
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
