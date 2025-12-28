3a9bffd8a134c195127b57ff149fab69b3df394e
SorerBOT <alonfiller@gmail.com>
2025-12-28 21:15:25 +0200

    FEAT added size table

---
+++ Ass2_DB/q2_2.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    # Made size_id the primary key...
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS size (
+                size_id INT PRIMARY KEY,
+                european_number TINYINT NOT NULL,
+                us_number TINYINT
+                )
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
