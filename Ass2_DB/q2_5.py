8495e3548180bf195bb743c5dc64be43227fef44
SorerBOT <alonfiller@gmail.com>
2025-12-28 21:48:59 +0200

    FEAT added countries table

---
+++ Ass2_DB/q2_5.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    # (shoe_id, size_id) combinations are unique
+    # shoe_id and size_id are foreign keys
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS country (
+            country_id INT PRIMARY KEY,
+            country_name VARCHAR(63) NOT NULL
+        )
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
