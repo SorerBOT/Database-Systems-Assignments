b553183da6de340a4c6c69cc6f5c896b7584d3c7
SorerBOT <alonfiller@gmail.com>
2025-12-28 21:51:38 +0200

    FEAT city table

---
+++ Ass2_DB/q2_6.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    # (shoe_id, size_id) combinations are unique
+    # shoe_id and size_id are foreign keys
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS city (
+            city_id INT PRIMARY KEY,
+            city_name VARCHAR(63) NOT NULL,
+            country_id INT NOT NULL,
+
+            FOREIGN KEY(country_id) country(country_id)
+
+        )
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
