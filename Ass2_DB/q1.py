374684dbfeb7680f5cd91204dac002b0671a0f05
SorerBOT <alonfiller@gmail.com>
2025-12-28 20:54:11 +0200

    FEAT added query to create database

---
+++ Ass2_DB/q1.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="mysql",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    cursor.execute("""
+        CREATE DATABASE IF NOT EXISTS biu_shoes;
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
