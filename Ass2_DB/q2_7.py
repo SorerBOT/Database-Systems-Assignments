621647b74bff561c433711f1a6465500e0ca4e9a
SorerBOT <alonfiller@gmail.com>
2025-12-28 22:00:31 +0200

    FEAT added customer table

---
+++ Ass2_DB/q2_7.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    # (shoe_id, size_id) combinations are unique
+    # shoe_id and size_id are foreign keys
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS customer (
+            customer_id VARCHAR(15) PRIMARY KEY,
+            first_name VARCHAR(31) NOT NULL,
+            last_name VARCHAR(31) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            city_id INT NOT NULL,
+
+            CHECK (LENGTH(customer_id) = 9),
+            FOREIGN KEY(city_id) REFERENCES city(city_id)
+        )
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
