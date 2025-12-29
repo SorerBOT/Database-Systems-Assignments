042a14392de95aabfcf94489d9dc98cafaeaed53
SorerBOT <alonfiller@gmail.com>
2025-12-29 19:21:00 +0200

    FEAT populated countries

---
+++ Ass2_DB/q3_5.py
@@ -0,0 +1,40 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    # (shoe_id, size_id) combinations are unique
+    # shoe_id and size_id are foreign keys
+
+    cursor.execute("""
+            INSERT INTO country (country_id, country_name)
+            VALUES
+                (1, 'Israel'),
+                (2, 'Canada'),
+                (3, 'United States'),
+                (4, 'Germany'),
+                (5, 'France'),
+                (6, 'Japan'),
+                (7, 'Australia'),
+                (8, 'Italy'),
+                (9, 'Spain'),
+                (10, 'Brazil')
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
