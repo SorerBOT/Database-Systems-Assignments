a887fe744873e864e146e9266a88d5f032b33063
SorerBOT <alonfiller@gmail.com>
2025-12-29 20:07:38 +0200

    FEAT first update

---
+++ Ass2_DB/q6_1.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+
+    cursor.execute("""
+        UPDATE size
+        SET uk_number = 5
+        WHERE european_number = 38;
+    """) 
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
