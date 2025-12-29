f8bbd95dd54e33f4b5c5638bf69a001f957528f9
SorerBOT <alonfiller@gmail.com>
2025-12-29 20:39:21 +0200

    FEAT added query to check how many sizes are available for each shoe

---
+++ Ass2_DB/q9.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+
+    cursor.execute("""
+        SELECT shoe.shoe_name, COUNT(shoe_size.shoe_id) FROM shoe
+        LEFT JOIN shoe_size on shoe.shoe_id = shoe_size.shoe_id
+        GROUP BY shoe.shoe_id;
+    """)
+
+    print (', '. join (str( row ) for row in cursor . fetchall ()))
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
