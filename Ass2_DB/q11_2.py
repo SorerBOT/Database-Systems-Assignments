4af97ad8dc8777f1037010e2a7ffca1d6e5a2d9f
SorerBOT <alonfiller@gmail.com>
2025-12-30 00:04:11 +0200

    FEAT written a query to get the data in the total_sales_per_shoe VIEW

---
+++ Ass2_DB/q11_2.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+
+    cursor.execute("""
+        SELECT * FROM total_sales_per_shoe;
+    """)
+
+    print (', '. join (str( row ) for row in cursor . fetchall ()))
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
