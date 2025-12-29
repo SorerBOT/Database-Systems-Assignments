aa7be6f979ff5ae191f4eef58e07138d5327f87f
SorerBOT <alonfiller@gmail.com>
2025-12-30 00:14:26 +0200

    FEAT added query to return all shoes which were never ordered

---
+++ Ass2_DB/q12.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+
+    cursor.execute("""
+        SELECT DISTINCT shoe.shoe_name as name
+        FROM shoe
+        LEFT JOIN order_shoe ON shoe.shoe_id = order_shoe.shoe_id
+        WHERE order_shoe.order_id IS NULL;
+    """)
+
+    print (', '. join (str( row ) for row in cursor . fetchall ()))
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
