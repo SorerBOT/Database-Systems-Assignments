a19ebc27c4bc9dda272ef086b91a2a3efeee1df0
SorerBOT <alonfiller@gmail.com>
2025-12-29 19:40:54 +0200

    FEAT populating order_shoe

---
+++ Ass2_DB/q3_9.py
@@ -0,0 +1,39 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    # (shoe_id, size_id) combinations are unique
+    # shoe_id and size_id are foreign keys
+
+    cursor.execute("""
+            INSERT INTO order_shoe (order_id, shoe_id)
+            VALUES
+                (1, 1),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+                (4, 5),
+                (5, 6),
+                (6, 7),
+                (7, 8),
+                (8, 9),
+                (9, 10)
+    """) 
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
