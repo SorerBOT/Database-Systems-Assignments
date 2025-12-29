b6e559d0fbb3142248e1ccc9c6b44302ef2b19a0
SorerBOT <alonfiller@gmail.com>
2025-12-29 19:38:19 +0200

    FEAT populated company_order

---
+++ Ass2_DB/q3_8.py
@@ -0,0 +1,44 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    # (shoe_id, size_id) combinations are unique
+    # shoe_id and size_id are foreign keys
+
+    cursor.execute("""
+            INSERT INTO company_order (order_id, order_date)
+            VALUES
+                (1, '2025-01-15 14:30:00'),
+                (2, '2025-02-10 10:15:00'),
+                (3, '2025-03-05 16:45:00'),
+                (4, '2025-04-12 09:00:00'),
+                (5, '2025-05-20 11:00:00'),
+                (6, '2025-06-01 13:30:00'),
+                (7, '2025-07-10 15:45:00'),
+                (8, '2025-08-15 17:00:00'),
+                (9, '2025-09-05 19:30:00'),
+                (10, '2025-10-22 08:15:00'),
+                (11, '2025-11-01 12:30:00'),
+                (12, '2025-12-10 14:45:00'),
+                (13, '2025-02-18 10:00:00'),
+                (14, '2025-03-20 08:15:00'),
+                (15, '2025-04-22 16:30:00')
+    """) 
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
