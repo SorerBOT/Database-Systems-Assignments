69fbba836c173973fbabbd8ba0b78569500fbb15
SorerBOT <alonfiller@gmail.com>
2026-01-15 14:27:07 +0200

    FEAT now creating the medicines table

---
+++ Ass3_DB/src/q3_4.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    # Establish the connection to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="Hosppdd",
+        port='3307',
+    )
+
+    # Create a cursor object to execute queries
+    cursor = mydb.cursor()
+
+    ## Execute the SQL query
+    ## medicines(id, name)
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS medicines (
+                    id INT PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL
+                );
+    """)
+
+    ## !!!Commit the transaction to save the changes to the database!!!
+    mydb.commit()
+
+    ## Close the cursor and the connection
+    cursor.close()
+    mydb.close()
